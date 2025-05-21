f2012eb0f6321ab9fa0ce275f5f92609237ca6c3
Avinash <avinashrana55@yahoo.com>
2025-05-21 18:19:59 +0530

    Added zod validation and register api

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import path from "path";
 import ejs from "ejs";
 import { fileURLToPath } from "url";
-import { sendEmail } from "./config/mail.js";
+import Routes from "./routes/index.js"
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
@@ -15,6 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 // * Set View engine
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./views"));
+
+// * Routes
+app.use(Routes);
 
 app.get("/", async (req: Request, res: Response) => {
     const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {
